3de2f8897c387fbca4687168826a6c3690b38a30
JasimAlmelhem <j_asim00@hotmail.com>
2017-01-05 09:37:09 -0500

    update auto update minor version to false for paragon servers

---
+++ Resources/testinstallmodule.ps1
@@ -0,0 +1,35 @@
+﻿Configuration MgmtServerConfig
+{
+    #Install-Module Azure -Force|Out-file c:\logs.txt
+    #Install-Azure
+      
+
+	Node localhost
+ 	{
+ 
+#Install-AzureRMPowershellModules
+ 		Script InstallAzureRMPowershellModules
+        	{
+	            SetScript = { 
+                    $script = Install-Module Azure
+                 }
+	            TestScript = { 
+                   $azurepsinstalled=get-wmiobject -class Win32_Product|where-object Name -like '*Azure Powershell*' 
+            
+                    if($Azurepsinstalled)
+                    {
+                        $True
+                    }
+                    else
+                    {
+                        $False
+                    }
+                }
+	            GetScript = {<# This must return a hash table #>}
+        	}
+
+    }#End of node
+
+}
+
+
