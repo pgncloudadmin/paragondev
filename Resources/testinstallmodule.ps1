f7437280caac5ac726fd92e9314ab1abc800a650
JasimAlmelhem <j_asim00@hotmail.com>
2017-01-05 13:23:49 -0500

    update prereqs for ntier, web

--- Resources/testinstallmodule.ps1
+++ Resources/testinstallmodule.ps1
@@ -1,35 +1,82 @@
 ﻿Configuration MgmtServerConfig
 {
-    #Install-Module Azure -Force|Out-file c:\logs.txt
-    #Install-Azure
+    param
+    (
+      [Parameter(Mandatory=$true)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Parameter(Mandatory=$true)]
+        [String]$StorageAcctKey,
+
+        [parameter(Mandatory=$false)] 
+        [String]$StorageAccountName="sairmcuploads01",
+
+        [parameter(Mandatory=$false)] 
+        [String]$StorageAccountContainer="test",
+
+        [parameter(Mandatory=$false)] 
+        [String]$artifactsLocation="https://raw.githubusercontent.com/djnavarrete/paragondev/master/Resources"
+
+    )
+
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, xAzureStorage
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
       
 
 	Node localhost
  	{
- 
+		Script TempDir
+        	{
+	            SetScript = 
+                    {  
+                        $trustrepo=Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+                        $install=install-module azure
+                        import-module azure 
+                    }
+	            GetScript =  { @{} }
+	            TestScript = { $false }
+                
+        	}
+    
 #Install-AzureRMPowershellModules
+<#
  		Script InstallAzureRMPowershellModules
         	{
 	            SetScript = { 
-                    $script = Install-Module Azure
+                    $trustrepo=Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+                    $install=install-module azure
+                    import-module azure
                  }
-	            TestScript = { 
-                   $azurepsinstalled=get-wmiobject -class Win32_Product|where-object Name -like '*Azure Powershell*' 
-            
-                    if($Azurepsinstalled)
-                    {
-                        $True
-                    }
-                    else
-                    {
-                        $False
-                    }
-                }
-	            GetScript = {<# This must return a hash table #>}
+	            TestScript = {$false}
+	            GetScript = {<# This must return a hash table #><# }
+                Credential = $DomainCreds
+               # DependsOn = "[Script]InstallWIPInstaller"
         	}
-
+<#Install using native Package  Did not install or return any errors
+        Package AzurePSPackage
+        {
+            Ensure = "Present"
+            Path ="C:\installs\WebpiCmd.exe"
+            ProductId = "64C3C1D5-870E-486B-BE4B-181256DC61F4"
+            Arguments =  "/Install /Products:'WindowsAzurePowershell' /AcceptEULA"
+            Name = "Microsoft Azure PowerShell - October 2015"
+            Credential = $DomainCreds
+        }
+        
+#>
+<#
+ xAzureBlobFiles Downloads {
+        Path                    = "C:\downloads"
+        StorageAccountName      = $StorageAccountName
+        StorageAccountContainer = $StorageAccountContainer
+        StorageAccountKey       = $StorageAcctKey
+        DependsOn = "[Script]InstallAzureRMPowershellModules"
+        }
+#>
     }#End of node
 
 }
-
-
